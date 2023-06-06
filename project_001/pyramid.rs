e98e7e9854c8ed7f82340903b63f484f50ba229d
Anselm Fernandez <anselm@outlook.com>
2023-06-06 19:25:22 -0400

    pyramid-by-gpt

---
+++ project_001/pyramid.rs
@@ -0,0 +1,35 @@
+use std::io;
+
+fn main() {
+    println!("Enter the number of rows for the pyramid:");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read input.");
+    let rows: u32 = input.trim().parse().expect("Invalid input. Please enter a positive integer.");
+
+    let mut row = 1;
+    let mut spaces = rows - 1;
+    let mut stars = 1;
+
+    while row <= rows {
+        let mut space_count = spaces;
+        let mut star_count = stars;
+
+        // Print spaces
+        while space_count > 0 {
+            print!(" ");
+            space_count -= 1;
+        }
+
+        // Print stars
+        while star_count > 0 {
+            print!("*");
+            star_count -= 1;
+        }
+
+        println!(); // Move to the next line
+
+        row += 1;
+        spaces -= 1;
+        stars += 2;
+    }
+}
